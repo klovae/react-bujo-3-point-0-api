4f787dc101d90e05e6d0305c4d2c41740aeae89f
klovae <elyse.klova@gmail.com>
2021-06-21 22:37:46 -0400

    updated models

--- app/models/day.rb
+++ app/models/day.rb
@@ -1,4 +1,16 @@
-class Task < ApplicationRecord
-  belongs_to :day
-  validates :content, presence: { message: "A task must have a description." }
+class Day < ApplicationRecord
+  has_many :tasks
+  has_many :migrations
+
+  def human_time
+    self.date.strftime("%A, %B %-d, %Y")
+  end
+
+  def self.today
+      find_or_create_by(date: Time.zone.today.beginning_of_day)
+  end
+
+  def self.tomorrow
+      find_or_create_by(date: Time.zone.tomorrow.beginning_of_day)
+  end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
   belongs_to :day
   has_many :migrations
+
+  validates :content, presence: { message: "A task must have a description." }
 end
