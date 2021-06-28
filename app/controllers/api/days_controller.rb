2e4e3dee2697007119c5f98f69f1a1dc1e7a6204
klovae <elyse.klova@gmail.com>
2021-06-28 10:44:02 -0400

    updated days and tasks controllers

--- app/controllers/api/days_controller.rb
+++ app/controllers/api/days_controller.rb
@@ -1,6 +1,8 @@
 class Api::DaysController < ApplicationController
 
   def index
+    Day.today
+    Day.tomorrow
     days = Day.all
     render json: DayWithTasksSerializer.new(days).to_serialized_json
   end
--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -1,7 +1,7 @@
 class Api::TasksController < ApplicationController
 
   def index
-    tasks = Task.all
+    tasks = Task.all.where(complete: false)
     render json: TasksSerializer.new(tasks).to_serialized_json
   end
 
