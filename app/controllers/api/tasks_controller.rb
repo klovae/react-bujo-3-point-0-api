be1e78cb49c56057f4ff057acb225d8de8a68aed
klovae <elyse.klova@gmail.com>
2021-06-28 23:24:38 -0400

    added task creation to TasksController

--- app/controllers/api/tasks_controller.rb
+++ app/controllers/api/tasks_controller.rb
@@ -5,4 +5,17 @@ class Api::TasksController < ApplicationController
     render json: TasksSerializer.new(tasks).to_serialized_json
   end
 
+  def create
+    task = Task.new(task_params)
+    if task.save
+      render json: TasksSerializer.new(task).to_serialized_json
+    end
+  end
+  
+  private
+
+  def task_params
+    params.require(:task).permit(:content, :day_id)
+end
+
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,5 @@ class Task < ApplicationRecord
   belongs_to :day
   has_many :migrations
 
-  validates :content, presence: { message: "A task must have a description." }
+  #validates :content, presence: { message: "A task must have a description." }
 end
